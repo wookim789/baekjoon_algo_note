022f0ae9ea479221d6ac3c15b971699720c9bc9e
wookim456@gmail.com <wookim789@gmail.com>
2020-09-08 22:56:59 +0900

    인접행렬 dfs

--- 타사이트_문제풀이/백준/address.py
+++ 타사이트_문제풀이/백준/address.py
@@ -1,27 +1,24 @@
 # 단지번호붙이기 2667
 
 # dfs로 모든 단지 순회
+# n = 7
+# map_tot = [[0,1,1,0,1,0,0],
+#             [0,1,1,0,1,0,1],
+#             [1,1,1,0,1,0,1],
+#             [0,0,0,0,1,1,1],
+#             [0,1,0,0,0,0,0],
+#             [0,1,1,1,1,1,0],
+#             [0,1,1,1,0,0,0]]
+# print(map_tot)
 
-# n = int(input())
-n = 7
 
+n = int(input())
 map_tot = []
-map_tot = [[0,1,1,0,1,0,0],
-            [0,1,1,0,1,0,1],
-            [1,1,1,0,1,0,1],
-            [0,0,0,0,1,1,1],
-            [0,1,0,0,0,0,0],
-            [0,1,1,1,1,1,0],
-            [0,1,1,1,0,0,0]]
-print(map_tot)
-# for i in range(n):
-#     map_tot.append(list(map(int, input())))
+for i in range(n):
+    map_tot.append(list(map(int, input())))
 
 visited = [[False] * n for _ in range(n)]
-
 result = []
-
-
 
 def dfs(y,x, visited, map_tot, count):
     if map_tot[y][x] == 0:
@@ -40,16 +37,13 @@ def dfs(y,x, visited, map_tot, count):
             count = dfs(y, x - 1, visited, map_tot, count)
     return count
 
-def print_map(map_tot):
-    for row in map_tot:
-        print(row)
-
-print_map(map_tot)
-
 for y in range(n):
     for x in range(n):
-        result.append(dfs(y,x, visited, map_tot, 0))
+        count = dfs(y,x, visited, map_tot, 0)
+        if  count > 0:
+            result.append(count)
         
 result.sort()
+print(len(result))
 for i in result:
     print(i)
