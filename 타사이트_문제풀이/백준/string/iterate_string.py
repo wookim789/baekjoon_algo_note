155d83d0dd7939b35c088690f024e261f8add5a8
wookim456@gmail.com <wookim789@gmail.com>
2020-09-15 22:10:22 +0900

    반복된 문자열

---
+++ 타사이트_문제풀이/백준/string/iterate_string.py
@@ -0,0 +1,12 @@
+# QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./:
+
+n = int(input())
+
+for i in range(n):
+    r, s = map(str, input().split())
+    r = int(r)
+
+    for j in s:
+        p = j * r
+        print(p, end = '')
+    print()
