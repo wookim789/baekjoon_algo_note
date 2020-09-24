abecb8c6e7674f6d2a74affdb3e00599a5e4706c
wookim456@gmail.com <wookim789@gmail.com>
2020-09-24 21:52:46 +0900

    체스 풀이 중

---
+++ tip/2matrix/search_all_matrixt_in_area.py
@@ -0,0 +1,42 @@
+# 2차원 리스트 m x n
+# 특정 범위 a x b (a < m, b < n)
+# 로 나누어 전부 순회
+
+y, x = map(int, input().split())
+
+m = []
+
+for r in range(y):
+    m.append(list(input()))
+
+cnt = 0
+for r_idx in range(y - 7):
+    for c_idx in range(x - 7):
+        for r in range(r_idx, r_idx + 8):
+            for c in range(c_idx, c_idx + 8):
+                print(m[r][c], end='')
+            print()
+        cnt += 1
+        print(cnt)
+        print()
+
+# 입력 예
+# 10 10
+#
+# wbwbwbwbwb
+# bwbwbwbwbw
+# bwbwbwbwbw
+# wbwbwbwbbw
+# bwbwbwbwbw
+# wbwbwbwbwb
+# bwbwbwbwbw
+# wbwbwbwbwb
+# bwbwbwbwbw
+# wbwbwbwbwb
+
+
+a = ['a','b','c']
+
+for i in a:
+    i = 'x'
+print(a)
--- 타사이트_문제풀이/백준/brotopos/ches.py
+++ 타사이트_문제풀이/백준/brotopos/ches.py
@@ -1,4 +1,5 @@
 # 체스판 칠하기
+import copy
 
 y, x = map(int, input().split())
 
@@ -7,11 +8,119 @@ m = []
 for r in range(y):
     m.append(list(input()))
 
+# 8 x 8 영역을 전부 순회
+# 좌측 상단 w or b 설정하여 칠해야 하는 수 설정
+# min으로 갱신
 
-# 모든 8 x 8 영역을 순회한다
+# W B
+
+# 2 for 로우 > 컬럼
+result = int(1e9)
+for r_idx in range(y - 7):
+    for c_idx in range(x - 7):
+        temp_arr = []
+        for r in range(r_idx, r_idx + 8):
+            temp_arr.append(m[r][c_idx : c_idx + 8])
+        # 좌측 상단의 색을 변경하여 한번더 검사를 위해
+        sec_temp_arr = copy.deepcopy(temp_arr)
+        left_top = temp_arr[0][0]
+        if left_top == 'W':
+            sec_temp_arr[0][0] = 'B'
+        else :
+            sec_temp_arr[0][0] = 'W'
+
+        left_first = ''
+        befor = ''
+        change_cnt = 0
+        first_col = 0
+        for r in range(8):
+            for c in range(8):
+                if left_first == '':
+                   left_first = temp_arr[r][c]
+                   befor = temp_arr[r][c]
+                   first_col += 1
+                   continue
+
+                if first_col == 0:
+                    if left_first == temp_arr[r][c]:
+                        if temp_arr[r][c] == 'B':
+                            temp_arr[r][c] = 'W'
+                        else:
+                            temp_arr[r][c] = 'B'
+                        change_cnt += 1
+                    first_col += 1
+                    left_first = temp_arr[r][c]
+                elif befor == temp_arr[r][c]:
+                    if temp_arr[r][c] == 'B':
+                        temp_arr[r][c] = 'W'
+                    else:
+                        temp_arr[r][c] = 'B'
+                    change_cnt += 1
+                befor = temp_arr[r][c]
+            first_col = 0
+        result = min(result, change_cnt)
+
+        
+        left_first = ''
+        befor = ''
+        change_cnt = 0
+        first_col = 0
+        for r in range(8):
+            for c in range(8):
+                if left_first == '':
+                   left_first = sec_temp_arr[r][c]
+                   befor = sec_temp_arr[r][c]
+                   first_col += 1
+                   continue
+
+                if first_col == 0:
+                    if left_first == sec_temp_arr[r][c]:
+                        if sec_temp_arr[r][c] == 'B':
+                            sec_temp_arr[r][c] = 'W'
+                        else:
+                            sec_temp_arr[r][c] = 'B'
+                        change_cnt += 1
+                    first_col += 1
+                    left_first = sec_temp_arr[r][c]
+                elif befor == sec_temp_arr[r][c]:
+                    if sec_temp_arr[r][c] == 'B':
+                        sec_temp_arr[r][c] = 'W'
+                    else:
+                        sec_temp_arr[r][c] = 'B'
+                    change_cnt += 1
+                befor = sec_temp_arr[r][c]
+            first_col = 0
+        result = min(result, change_cnt)
+print(result)
+# wbwbwbwbwb
+# bwbwbwbwbw
+# bwbwbwbwbw
+# wbwbwbwbbw
+# bwbwbwbwbw
+# wbwbwbwbwb
+# bwbwbwbwbw
+# wbwbwbwbwb
+# bwbwbwbwbw
+# wbwbwbwbwb
+# 8 8
+# WBWBWBWB
+# BWBWBWBW
+# WBWBWBWB
+# BWBBBWBW
+# WBWBWBWB
+# BWBWBWBW
+# WBWBWBWB
+# BWBWBWBW
 
 
-# 해당 영역에 바꿔야 하는 칸의 수를 센다
-cnt = 0
-for r in range(0, y - 7):
-    for c in range(0, x - 7):
+# 10 13
+# BBBBBBBBWBWBW
+# BBBBBBBBBWBWB
+# BBBBBBBBWBWBW
+# BBBBBBBBBWBWB
+# BBBBBBBBWBWBW
+# BBBBBBBBBWBWB
+# BBBBBBBBWBWBW
+# BBBBBBBBBWBWB
+# WWWWWWWWWWBWB
+# WWWWWWWWWWBWB
