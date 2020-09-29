9f8c85d7c1aa6a6dd231369aaaf98c3315b4c9f4
wookim456@gmail.com <wookim789@gmail.com>
2020-09-29 12:45:23 +0900

    트리의 지름 시간초과

--- 타사이트_문제풀이/백준/tree/tree_ex2.py
+++ 타사이트_문제풀이/백준/tree/tree_ex2.py
@@ -1,51 +1,43 @@
 # 트리의 지름
 
 import sys
+sys.setrecursionlimit(200000)
 
 n = int(sys.stdin.readline())
 
-edges = [[]]
-for i in range(n):
-    edges.append(list(map(int, sys.stdin.readline().split())))
-print(edges)
-matrix = [[-1 for _ in range(n)] for _ in range(n)]
+edges = [[] for _ in range(n + 1)]
+tot = []
+idx_set = set()
 
-for i in range(n):
-    matrix[i][i] = 0
+for _ in range(n):
+    tmp = list(map(int, sys.stdin.readline().split()))
+    idx = tmp[0]
+    idx_set.add(idx)
+    e_idx = 1
+    while tmp[e_idx] != -1:
+        edges[idx].append(tuple(tmp[e_idx : e_idx + 2]))
+        e_idx += 2
 
-for i in range(1, n + 1):
-    edge = edges[i]
-    mt_idx = edge[0] - 1
+def sum_cost_recur(w_list, cost, n_cost, c_idx):
+    global edges, tot
 
-    for e_idx in range(1, len(edge), 2):
-        if edge[e_idx] != -1:
-            matrix[mt_idx][edge[e_idx]-1] = edge[e_idx + 1]
-print(matrix)
+    if c_idx in w_list:
+        if (tot and max(tot) < cost - n_cost) or not tot:
+            tot.append(cost - n_cost)
+        return
 
+    w_list.append(c_idx)
+
+    for n_idx, n_cost in edges[c_idx]:
+        sum_cost_recur(w_list, cost + n_cost, n_cost, n_idx)
+
+for idx in idx_set:
+        sum_cost_recur([], 0, 0, idx)
+    
+print(max(tot))
 # 1 3 2 -1
 # 2 4 4 -1
 # 3 1 2 4 3 -1
 # 4 2 4 3 3 5 6 -1
 # 5 4 6 -1
 
-recur_stack = []
-root = 0
-cur = 0
-length = 0
-recur_stack.append([root, cur, length])
-
-while recur_stack:
-    data = recur_stack.pop()
-    root, cur, length = data[0], data[1], data[2]
-    idx = 0
-    for col in matrix[root]:
-        if col != 0 and col != -1 :
-            length += col
-            recur_stack.append([root, idx, length])
-        idx += 1
-
-# [[ 0, -1,  2, -1, -1], 
-#  [-1,  0, -1,  4, -1], 
-#  [ 2, -1,  0,  3, -1], 
-#  [-1,  4,  3,  0,  6], 
-#  [-1, -1, -1,  6,  0]]
