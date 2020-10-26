62511a3434946e7a218afb69027c59e31c6ab839
wookim456@gmail.com <wookim789@gmail.com>
2020-10-26 16:09:45 +0900

    리트코드

---
+++ 타사이트_문제풀이/leetcode/tow_sum.py
@@ -0,0 +1,15 @@
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        for f in range(0, len(nums) - 1):
+            for b in range(f + 1, len(nums)):
+                if nums[f] + nums[b] == target:
+                    return [f, b]
+
+
+sol = Solution()
+
+print(sol.twoSum([3,2,4], 6))
